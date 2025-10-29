0042237eae8679197635ecb11b21c5722446af19
rjfredr <fredrickalexander1727@gmail.com>
2025-10-28 23:58:18 -0700

    completed video arg processor and basic command calling

---
+++ src/main/java/io/github/rjrico17/centroidfinder/App.java
@@ -0,0 +1,7 @@
+package io.github.rjrico17.centroidfinder;
+
+public class App {
+    public static void main(String args[]) throws Exception {
+        VideoArgProcessor config = new VideoArgProcessor(args);
+    }
+}
