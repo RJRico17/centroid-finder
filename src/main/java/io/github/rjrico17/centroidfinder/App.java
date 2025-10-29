400f409466b57d56017c174ce6a0e904ffed3559
rjfredr <fredrickalexander1727@gmail.com>
2025-10-29 11:28:57 -0700

    main logic completed(?)

--- src/main/java/io/github/rjrico17/centroidfinder/App.java
+++ src/main/java/io/github/rjrico17/centroidfinder/App.java
@@ -1,6 +1,9 @@
 package io.github.rjrico17.centroidfinder;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.imageio.ImageIO;
 
 public class App {
@@ -10,12 +13,25 @@ public class App {
         ColorDistanceFinder distanceFinder = new EuclideanColorDistance();
         ImageBinarizer binarizer = new DistanceImageBinarizer(distanceFinder, config.getTargetColor(), config.getTargetColor());
         
-        int[][] binarized = binarizer.toBinaryArray(ImageIO.read(new File(config.getInputPath())));
-        BufferedImage buffered = binarizer.toBufferedImage(binarized);
+        // int[][] binarized = binarizer.toBinaryArray(ImageIO.read(new File(config.getInputPath())));
+        // BufferedImage buffered = binarizer.toBufferedImage(binarized);
 
         ImageGroupFinder groupFinder = new BinarizingImageGroupFinder(binarizer, new DfsBinaryGroupFinder());
 
-        
 
+        List<BufferedImage> frames = VideoProcessor.processVideo(config.getInputPath());
+        try (PrintWriter writer = new PrintWriter("groups.csv")) {
+            for (BufferedImage img : frames) {
+                List<Group> groups = groupFinder.findConnectedGroups(img);
+                for (Group group : groups) {
+                    writer.println(group.toCsvRow());
+                }
+            }
+            System.out.println("Groups summary saved as groups.csv");
+        } catch (Exception e) {
+            System.err.println("Error writing groups.csv");
+            e.printStackTrace();
+        }
+        
     }
 }
