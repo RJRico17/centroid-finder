c5da226d5430f530af353414af574f9fb8c8a010
rjfredr <fredrickalexander1727@gmail.com>
2025-10-29 00:01:06 -0700

    video arg processor

--- src/main/java/io/github/rjrico17/centroidfinder/App.java
+++ src/main/java/io/github/rjrico17/centroidfinder/App.java
@@ -3,5 +3,9 @@ package io.github.rjrico17.centroidfinder;
 public class App {
     public static void main(String args[]) throws Exception {
         VideoArgProcessor config = new VideoArgProcessor(args);
+
+        ColorDistanceFinder distanceFinder = new EuclideanColorDistance();
+        ImageBinarizer binarizer = new DistanceImageBinarizer(distanceFinder, config.getTargetColor(), config.getTargetColor());
+        ImageGroupFinder groupFinder = new BinarizingImageGroupFinder(binarizer, new DfsBinaryGroupFinder());
     }
 }
