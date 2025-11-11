823f7604af41c010273c536d63329391e09b2124
rjfredr <fredrickalexander1727@gmail.com>
2025-11-10 17:47:21 -0800

    right logic for process video it just cannot find the jar

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 import { exec } from 'child_process';
 import fs from 'fs';
+import { stderr, stdout } from 'process';
+import crypto from 'crypto';
+import { URLSearchParams } from 'url';
 
 dotenv.config()
 const { ROUTE } = process.env;
@@ -19,7 +22,9 @@ export const getThumbnail = (req,res) => {
     exec(`ffmpeg -i videos/${file} -frames:v 1 thumbnail.jpeg`, (err, stdout, stderr) => {
         if (err) {
             console.log(err);
-            return;
+            return {
+                "error": err
+            }
         }
         if (stdout) {
             console.log(stdout);
@@ -28,8 +33,30 @@ export const getThumbnail = (req,res) => {
         console.log(stderr);
     })
 }
-export const processVideo = (req,res) => {
-    
+
+export const processVideo = async (req,res) => {
+    // const query = window.location.search;
+    const file = req.params.filename;
+    // const targetColor = req.params.targetColor;
+    // const threshold = req.params.threshold;
+    const targetColor = req.query.targetColor;
+    const threshold = req.query.threshold;
+    await exec(`java -jar ../processor/target/centroid-finder-1.0-SNAPSHOT.jar ${file} output.csv ${targetColor} ${threshold}`, (err, stdout, stderr) => {
+        if (err) {
+            console.log(err);
+            return {
+                "error": err
+            }
+        }
+        if (stdout) {
+            console.log(stdout);
+            const uuid = crypto.randomUUID();
+            return {
+                "jobId": uuid
+            }
+        }
+        console.log(stderr);
+    })
 }
 export const getStatus = (req,res) => {
     
