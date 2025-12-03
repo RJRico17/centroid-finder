4420f1cee0c968d1deb3fbb9e6156afdcba0452c
rjfredr <fredrickalexander1727@gmail.com>
2025-12-03 09:21:17 -0800

    enable cors, restored thumbnail, need to reinstall all depenceies again

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import salamanderRouter from './routes.js';
 
+import cors from "cors";
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cors());
 
 app.use('/', salamanderRouter);
 
